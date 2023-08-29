b17d4f49f3d61e73e8053ba13815e46624b97328
SeungHyunLee054 <yellow13lsh@naver.com>
2023-08-30 00:25:53 +0900

    001-get-movie-data restTemplate 수정

--- movie-api/src/main/java/com/zerobase/service/KobisOpenAPIService.java
+++ movie-api/src/main/java/com/zerobase/service/KobisOpenAPIService.java
@@ -1,16 +1,21 @@
 package com.zerobase.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zerobase.domain.MovieDto;
 import com.zerobase.domain.model.Movie;
+import com.zerobase.exception.CustomException;
+import com.zerobase.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -23,14 +28,37 @@ public class KobisOpenAPIService {
     private final String API_MOVIE_DETAIL_URL = "searchMovieInfo.json?key=";
     private final String KEY = System.getProperty("apiKey");
 
-    public JSONObject getMovies(String page) {
+    public JsonNode getMovieJson(String page) {
         String strUrl = API_BASE_URL
                 + API_MOVIES_URL
                 + KEY
                 + "&curPage=" + page
                 + "&itemPerPage=100";
 
-        return restTemplate.getForEntity(strUrl, JSONObject.class).getBody();
+        ResponseEntity<String> response = restTemplate.getForEntity(strUrl, String.class);
+        JsonNode root = null;
+        try {
+            root = mapper.readTree(response.getBody());
+        } catch (JsonProcessingException e) {
+
+        }
+
+        if (root == null) {
+            throw new CustomException(ErrorCode.WRONG_URL);
+        }
+
+        JsonNode movieListResult = root.get("movieListResult");
+
+        return movieListResult;
+    }
+
+    public List<Movie> getMovieList(String page) {
+        JsonNode movieList = getMovieJson(page).get("movieList");
+        List list = mapper.convertValue(movieList, List.class);
+        List<MovieDto> movieDtos = list.stream().map(x -> mapper.convertValue(x, MovieDto.class)).toList();
+        List<Movie> movies = movieDtos.stream().map(MovieDto::from).toList();
+
+        return movies;
     }
 
     public JSONObject getMovieDetail(String movieCd) {
@@ -42,37 +70,8 @@ public class KobisOpenAPIService {
         return restTemplate.getForEntity(strUrl, JSONObject.class).getBody();
     }
 
-    public List<Movie> parseMovies(JSONObject jsonObject) {
-        List<Movie> movies = new ArrayList<>();
-        JSONObject movieListResult = mapper.convertValue(jsonObject.get("movieListResult"), JSONObject.class);
-        JSONArray movieList = mapper.convertValue(movieListResult.get("movieList"), JSONArray.class);
-        for (Object value : movieList) {
-            JSONObject object = mapper.convertValue(value, JSONObject.class);
-            JSONArray directors = mapper.convertValue(object.get("directors"), JSONArray.class);
-            String director = jsonArrayToStringDirectorsAndActors(new StringBuilder(), directors);
-
-            JSONArray companys = mapper.convertValue(object.get("companys"), JSONArray.class);
-            String company = jsonArrayToStringCompanys(new StringBuilder(), companys);
-
-            movies.add(Movie.builder()
-                    .movieCd((String) object.get("movieCd"))
-                    .movieName((String) object.get("movieNm"))
-                    .movieNameEn((String) object.get("movieNmEn"))
-                    .prdtYear((String) object.get("prdtYear"))
-                    .openDt((String) object.get("openDt"))
-                    .typeName((String) object.get("typeNm"))
-                    .prdtStatNm((String) object.get("prdtStatNm"))
-                    .nationAlt((String) object.get("nationAlt"))
-                    .genreAlt((String) object.get("genreAlt"))
-                    .directorName(director)
-                    .companyName(company)
-                    .build());
-        }
-        return movies;
-    }
-
-    public int totalCnt(JSONObject jsonObject) {
-        return (int) mapper.convertValue(jsonObject.get("movieListResult"), JSONObject.class).get("totCnt");
+    public int totalCnt(JsonNode movieListResult) {
+        return mapper.convertValue(movieListResult.get("totCnt"), Integer.class);
     }
 
     private String jsonArrayToStringDirectorsAndActors(StringBuilder sb, JSONArray jsonArray) {
