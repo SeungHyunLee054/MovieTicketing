2709b7b493b0a3cfc3f41e78fbd3f8911d0b44fc
SeungHyunLee054 <yellow13lsh@naver.com>
2023-09-08 20:37:44 +0900

    003-quartz-scheduler 수정

--- movie-api/src/main/java/com/zerobase/controller/MovieController.java
+++ movie-api/src/main/java/com/zerobase/controller/MovieController.java
@@ -18,18 +18,18 @@ public class MovieController {
         return ResponseEntity.ok("영화 데이터 저장 성공");
     }
 
-    @GetMapping("/search/detail")
+    @GetMapping("/detail")
     public ResponseEntity<MovieDetailDto> searchMovieDetail(@RequestParam String movieCd) {
         return ResponseEntity.ok(movieService.searchMovieDetail(movieCd));
     }
 
-    @PostMapping("/open/save")
+    @PostMapping("/open")
     public ResponseEntity<String> saveOpenMovies() {
         movieService.saveOpenMovies();
         return ResponseEntity.ok("상영중 영화 데이터 저장 성공");
     }
 
-    @PostMapping("/open/delete")
+    @DeleteMapping("/open")
     public ResponseEntity<String> deletePastOpenMovie() {
         movieService.deletePastOpenMovies();
         return ResponseEntity.ok("상영 종료 영화 데이터 삭제 성공");
