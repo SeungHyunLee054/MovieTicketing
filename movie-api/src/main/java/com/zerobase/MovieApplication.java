a6903254e2ad88928f0918cdd6475095d98118d5
SeungHyunLee054 <yellow13lsh@naver.com>
2023-09-08 19:01:43 +0900

    003-quartz-scheduler

--- movie-api/src/main/java/com/zerobase/MovieApplication.java
+++ movie-api/src/main/java/com/zerobase/MovieApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieApplication {
-//    @Autowired
-//    private JobDetail jobDetail;
-
     public static void main(String[] args) {
         SpringApplication.run(MovieApplication.class, args);
     }
